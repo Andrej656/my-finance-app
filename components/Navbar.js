8d896e38b57d66b72b2e1756fa050567d76b4278
btcdev <chaindev@icloud.com>
2024-05-22 12:52:36 +0200

    edit

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <a className="text-white">MyFinanceApp</a>
+        <Link href="/" className="text-white">
+          MyFinanceApp
         </Link>
         <div>
-          <Link href="/trade">
-            <a className="text-gray-300 hover:text-white ml-4">Trade</a>
+          <Link href="/trade" className="text-gray-300 hover:text-white ml-4">
+            Trade
           </Link>
         </div>
       </div>
